55ddd43d676cb67e7f74f645af9bd47e379e5742
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-10 12:03:39 -0500

    changing identation in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules.
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+  devise  :database_authenticatable, :registerable, :recoverable, :rememberable,
+          :trackable, :validatable, :confirmable, :omniauthable
 
 <<<<<<< HEAD
   validates :first_name, :last_name, presence: true
 =======
   include DeviseTokenAuth::Concerns::User
-  
   validates :first_name, presence: true
   validates :last_name, presence: true
 >>>>>>> Configuring devise_token_auth making rollback
