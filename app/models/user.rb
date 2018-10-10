bb5b12170b573e0caa87bcd95e25b20290706f09
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-10 12:03:39 -0500

    changing identation in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules.
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+  devise  :database_authenticatable, :registerable, :recoverable, :rememberable,
+          :trackable, :validatable, :confirmable, :omniauthable
 
   include DeviseTokenAuth::Concerns::User
-  
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
