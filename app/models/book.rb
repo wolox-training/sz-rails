25d21d306e4bf001f24b4b5db36f78a053572178
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-11 16:10:01 -0500

    rubocop changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,4 @@
 class Book < ApplicationRecord
-  has_many :rents
-  
+  has_many :rents, dependent: :destroy
   validates :genre, :autor, :image, :title, :editor, :year, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
           :trackable, :validatable, :confirmable, :omniauthable
 
   include DeviseTokenAuth::Concerns::User
+  has_many :rents, dependent: :destroy
 
-  has_many :rents
-  
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
