270fde847b5a73f786eac32bffdfd1a01aee2ab7
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-12 15:05:45 -0500

    changing singleton notation for BooksController

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,26 +1,30 @@
-class Api::V1::BooksController < ApplicationController
-  before_action :set_book, only: [:show]
+module Api
+  module V1
+    class BooksController < ApplicationController
+      before_action :set_book, only: [:show]
 
-  def index
-    @books = Book.all
-    render_paginated @books, each_serializer: BookSerializer
-  end
+      def index
+        @books = Book.all
+        render_paginated @books, each_serializer: BookSerializer
+      end
 
-  def show
-    @book = set_book
+      def show
+        @book = set_book
 
-    if @book
-      render json: @book, serializer: BookSerializer
-    else
-      render json: {
-        error: 'There is no book.'
-      }, status: :internal_server_error
+        if @book
+          render json: @book, serializer: BookSerializer
+        else
+          render json: {
+            error: 'There is no book.'
+          }, status: :internal_server_error
+        end
+      end
+
+      private
+
+      def set_book
+        Book.find_by(id: params[:id])
+      end
     end
-  end
-
-  private
-
-  def set_book
-    Book.find_by(id: params[:id])
   end
 end
