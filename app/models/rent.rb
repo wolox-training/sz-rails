dc8a3df681965ead6074b39827c1440c466fa523
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-22 09:49:12 -0500

    validate dates

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -3,4 +3,23 @@ class Rent < ApplicationRecord
   belongs_to :book, optional: true
 
   validates :start_date, :end_date, presence: true
+  validate :check_dates
+
+  def valid_dates?
+    in_range? && today_valid?
+  end
+
+  def in_range?
+    start_date < end_date
+  end
+
+  def today_valid?
+    start_date < Time.zone.today && Time.zone.today < end_date
+  end
+
+  private
+
+  def check_dates
+    errors.add(:model_rent, 'dates do not meet the requirements.') unless valid_dates?
+  end
 end
