4a8cbbcb876ac475fc3e0d3758b326e2a6784071
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-24 11:38:41 -0500

    making pilot index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   api_version(module: 'api/v1', path: { value: 'api/v1' }) do
     mount_devise_token_auth_for 'User', at: 'auth'
     resources :books, only: [:index, :show]
+    resources :rents, only: [:index, :create]
   end
   root "application#index"
 end
