1453f04458959f5b35e786d883a16b85e05cd09b
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-24 18:17:54 -0500

    rent validation

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -3,34 +3,34 @@ class Rent < ApplicationRecord
   belongs_to :book, optional: true
 
   validates :start_date, :end_date, presence: true
-  validate :check_creation
+
+  validate :in_range?, :check_creation
 
   scope :from_today, -> { where('DATE(?) BETWEEN start_date AND end_date', Time.zone.today) }
 
-  def reserve_conflict?
-    if book_id
-      rents = Rent.from_today.where(book_id: book_id)
-      rents.any?
-    else
-      false
-    end
+  def self.overlapping_ranges(book_id, start_date, end_date)
+    where(book_id: book_id).where(
+      "(:start_date >= start_date AND :start_date <= end_date) OR
+      (:end_date >= start_date AND :end_date <= end_date) OR
+      (:start_date <= start_date AND :end_date >= end_date)",
+      start_date: start_date,
+      end_date: end_date
+    )
   end
 
-  def valid_dates?
-    in_range? && today_valid?
-  end
+  protected
 
-  def in_range?
-    start_date < end_date
-  end
-
-  def today_valid?
-    start_date < Time.zone.today && Time.zone.today < end_date
+  def overlating_conflicts?
+    book_id.nil? || Rent.overlapping_ranges(book_id, start_date, end_date).count.zero?
   end
 
   private
 
   def check_creation
-    errors.add(:model_rent, 'dates creation error') unless valid_dates? && !reserve_conflict?
+    errors.add(:model_rent, 'dates creation error') unless overlating_conflicts?
+  end
+
+  def in_range?
+    errors.add(:model_rent, 'dates range error') unless start_date < end_date
   end
 end
