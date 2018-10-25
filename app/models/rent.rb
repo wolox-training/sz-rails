d59267c64a36b72b0ab3ec167a50a32581a0623b
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-25 16:37:02 -0500

    changing function by scope

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -4,11 +4,11 @@ class Rent < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
 
-  validate :in_range?, :check_creation
+  validate :in_range, :check_creation
 
   scope :from_today, -> { where('DATE(?) BETWEEN start_date AND end_date', Time.zone.today) }
 
-  def self.overlapping_ranges(book_id, start_date, end_date)
+  scope :overlapping_ranges, lambda { |book_id, start_date, end_date|
     where(book_id: book_id).where(
       "(:start_date >= start_date AND :start_date <= end_date) OR
       (:end_date >= start_date AND :end_date <= end_date) OR
@@ -16,21 +16,21 @@ class Rent < ApplicationRecord
       start_date: start_date,
       end_date: end_date
     )
-  end
+  }
 
   protected
 
-  def overlating_conflicts?
+  def overlapping?
     book_id.nil? || Rent.overlapping_ranges(book_id, start_date, end_date).count.zero?
   end
 
   private
 
   def check_creation
-    errors.add(:model_rent, 'dates creation error') unless overlating_conflicts?
+    errors.add(:model_rent, 'dates creation error') unless overlapping?
   end
 
-  def in_range?
+  def in_range
     errors.add(:model_rent, 'dates range error') unless start_date < end_date
   end
 end
