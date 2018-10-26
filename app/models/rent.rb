731460ec2bdaf330a5d17fe0e0fbdfdb77b5f916
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-26 11:41:35 -0500

    adding id for update object range

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -8,8 +8,8 @@ class Rent < ApplicationRecord
 
   scope :from_today, -> { where('DATE(?) BETWEEN start_date AND end_date', Time.zone.today) }
 
-  scope :overlapping_ranges, lambda { |book_id, start_date, end_date|
-    where(book_id: book_id).where(
+  scope :overlapping_ranges, lambda { |id, book_id, start_date, end_date|
+    where(book_id: book_id).where.not(id: id).where(
       "(:start_date >= start_date AND :start_date <= end_date) OR
       (:end_date >= start_date AND :end_date <= end_date) OR
       (:start_date <= start_date AND :end_date >= end_date)",
@@ -21,7 +21,7 @@ class Rent < ApplicationRecord
   protected
 
   def overlapping?
-    book_id.nil? || Rent.overlapping_ranges(book_id, start_date, end_date).count.zero?
+    book_id.nil? || Rent.overlapping_ranges(id, book_id, start_date, end_date).count.zero?
   end
 
   private
