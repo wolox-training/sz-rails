66a482f2675b0c686353b5777d2103616966f48a
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-26 14:25:29 -0500

    creating rents

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
 
   def index; end
 
-  def index; end
-
   def not_found
     render json: { error: "record not found with id #{params[:id]}" }, status: :not_found
   end
