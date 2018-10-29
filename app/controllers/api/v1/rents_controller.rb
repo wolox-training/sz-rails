67a07fc28114d2861a6ea0e4d50dfa9c01fe5640
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-29 17:32:03 -0500

    adding mailer with delay in rent creation endpoint

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -8,7 +8,9 @@ module Api
       end
 
       def create
-        render json: Rent.create!(rent_params), status: :created
+        rent = Rent.create!(rent_params)
+        GeneralMailer.finish_rent(rent).deliver_later(wait: 10.seconds)
+        render json: rent, status: :created
       end
 
       private
