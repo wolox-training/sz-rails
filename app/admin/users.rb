6c3cda93ee0c85270472da68f4004be39ae61a02
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-10-31 14:44:40 -0500

    creating users administratotr

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,14 +1,49 @@
 ActiveAdmin.register User do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :email, :password, :password_confirmation, :first_name, :last_name, :locale
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :first_name
+    column :last_name
+    column :locale
+    actions
+  end
+
+  filter :email
+  filter :first_name
+  filter :last_name
+  filter :locale
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :first_name
+      f.input :last_name
+      f.input :locale, as: :select, collection: %w[en es]
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+      row :first_name
+      row :last_name
+      row :locale
+      row :created_at
+      row :updated_at
+      row :sign_in_count
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :current_sign_in_ip
+      row :last_sign_in_ip
+      row :confirmed_at
+      row :confirmation_sent_at
+      row :unconfirmed_email
+    end
+  end
 end
