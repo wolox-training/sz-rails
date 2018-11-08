d6366dccaabd04301c2a3853770cd0f4700ced56
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-11-08 17:50:53 -0500

    changing render root json with new configuration on serializers.rb

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -3,7 +3,7 @@ module Api
     class BookSuggestionsController < ApiController
       def create
         book_suggestion = BookSuggestion.create!(book_suggestions_params)
-        render json: book_suggestion, adapter: :json, status: :created
+        render json: book_suggestion, root: 'book_suggestion', status: :created
       end
 
       private
--- app/serializers/book_suggestion_serializer.rb
+++ app/serializers/book_suggestion_serializer.rb
@@ -1,7 +1,3 @@
 class BookSuggestionSerializer < ActiveModel::Serializer
   attributes :title, :author, :link
-
-  def json_key
-    'book_suggestion'
-  end
 end
