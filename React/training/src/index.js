2922c551435020a47aca35478d5393feed41aa9b
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-04 10:58:59 -0500

    adding new folder distribution by Harry advices

--- React/training/src/index.js
+++ React/training/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './scss/index.scss';
+import Game from './app/screens/Game';
+import * as serviceWorker from './services/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
