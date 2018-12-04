13ac230c8c823cabf6958744086c615adb565560
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-04 15:34:15 -0500

    Adding onClick function to square button

--- React/training/src/app/screens/Game/components/Square/index.js
+++ React/training/src/app/screens/Game/components/Square/index.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './styles.scss';
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={ () => { this.setState({value: 'x'}) } }>
+        {this.state.value}
       </button>
     );
   }
