843a2a6db30b7eb6afc4f14eb01e53c8ce961a10
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-05 10:05:26 -0500

    declaring winner

--- React/training/src/app/screens/Game/components/Board/index.js
+++ React/training/src/app/screens/Game/components/Board/index.js
@@ -11,20 +11,6 @@ class Board extends Component {
     }
   }
 
-  nextPlayer() {
-    return this.state.xIsNext ? 'X' : 'O';
-  }
-
-  handleClick(i) {
-    console.log('Board-handleClick: ', i, ' xIsNext: ', this.state.xIsNext);
-    const squares = this.state.squares.slice();
-    squares[i] = this.nextPlayer();
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square 
@@ -34,8 +20,54 @@ class Board extends Component {
     )
   }
 
+  nextPlayer() {
+    return this.state.xIsNext ? 'X' : 'O';
+  }
+
+  handleClick(i) {
+    console.log('Board-handleClick: ', i, ' xIsNext: ', this.state.xIsNext);
+    const squares = this.state.squares.slice();
+
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.nextPlayer();
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const status = 'Next player: ' + this.nextPlayer();
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + this.nextPlayer();
+    }
 
     return (
       <Fragment>
