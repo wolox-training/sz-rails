e411531a7e95040eb4000320bfd6317877af35fc
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-05 11:00:33 -0500

    adding first aprox to history on game

--- React/training/src/app/screens/Game/components/Board/index.js
+++ React/training/src/app/screens/Game/components/Board/index.js
@@ -3,75 +3,18 @@ import Square from './../Square';
 import styles from './styles.module.scss';
 
 class Board extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
-
   renderSquare(i) {
     return (
-      <Square 
-        value={ this.state.squares[i] }
-        onClick={ () => this.handleClick(i) }
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
 
-  nextPlayer() {
-    return this.state.xIsNext ? 'X' : 'O';
-  }
-
-  handleClick(i) {
-    console.log('Board-handleClick: ', i, ' xIsNext: ', this.state.xIsNext);
-    const squares = this.state.squares.slice();
-
-    if (this.calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.nextPlayer();
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
-  calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
   render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status;
-
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + this.nextPlayer();
-    }
-
     return (
       <Fragment>
-        <div className={styles.status}>{status}</div>
         <div className={styles.boardRow}>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- React/training/src/app/screens/Game/index.js
+++ React/training/src/app/screens/Game/index.js
@@ -3,14 +3,81 @@ import Board from './components/Board';
 import styles from './styles.module.scss';
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    console.log('Game-handleClick: ', i, ' xIsNext: ', this.state.xIsNext);
+
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.nextPlayer();
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  nextPlayer() {
+    return this.state.xIsNext ? 'X' : 'O';
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = this.calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + this.nextPlayer();
+    }
+
     return (
       <div className={styles.game}>
         <div className={styles.gameBoard}>
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className={styles.gameInfo}>
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
