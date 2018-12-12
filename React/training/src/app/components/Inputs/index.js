3b6e122e28aa67516c151cb1237e2ce96f0c8a04
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-12 17:30:51 -0500

    form styles without modules

--- React/training/src/app/components/Inputs/index.js
+++ React/training/src/app/components/Inputs/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import cx from 'classnames';
+import './styles.scss';
+
+const getValidityClassName = meta => {
+  if (meta.active) {
+    return;
+  }
+  if (meta.touched && meta.invalid) {
+    return 'invalid';
+  }
+  if (meta.touched && meta.valid) {
+    return 'valid';
+  }
+};
 
 export const customInput = props => {
   const { label, input, type, meta } = props;
   return (
-    <div>
+    <div className={ cx(
+        'input',
+        { dirty: meta.dirty },
+        getValidityClassName(meta)
+      )}>
       <label>{props.label}</label>
       <input {...props.input} type={props.type} />
-      {meta.error && meta.touched && (
-        <div style={{ color: 'red' }}>{meta.error}</div>
+      { meta.error && meta.touched && !meta.active && (
+        <div className="error">
+          {meta.error}
+        </div>
       )}
     </div>
   );
--- React/training/src/app/screens/Login/components/Form/index.js
+++ React/training/src/app/screens/Login/components/Form/index.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { customInput } from './../../../../components/Inputs';
 import { required, minLength, validEmail } from '../../../../../utils/LoginFormValidation';
+import './styles.scss';
 
 class Form extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={'login'}>
         <Field name='email' component={customInput} type='email' label='Email' validate={[required, validEmail]} />
         <Field name='password' component={customInput} type='password' label='Password' validate={[required, minLength]} />
-        <button type='submit'>Login</button>
+        <button type='submit' className={'blue'}>Login</button>
       </form>
     );
   }
