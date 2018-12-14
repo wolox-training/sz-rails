c6f301209078287cfea249dcf843e2ab73ab2cd0
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-14 09:20:52 -0500

    removing utils for user

--- React/training/src/utils/userUtils.js
+++
@@ -1,8 +0,0 @@
-export function checkUser(users, user) {
-  for (let i = 0; i < users.length; i++) {
-    if ( (users[i].email.toLowerCase() === user.email.toLowerCase()) && (users[i].password === user.password) ) {
-      return true;
-    }
-  }
-  return false;
-}
