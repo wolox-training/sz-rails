3413c4a220976ce6dd5ac99c4d1afa65f2ef6484
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-17 13:57:22 -0500

    changing NavLink by Link without active link

--- React/training/src/app/components/Topbar/index.js
+++ React/training/src/app/components/Topbar/index.js
@@ -3,28 +3,20 @@ import { connect } from 'react-redux';
 import styles from './styles.module.scss';
 import logo from './../../../assets/react_icon.svg';
 import authActions from './../../../redux/Auth/actions';
-import { NavLink } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 class Topbar extends Component {
-  isActiveFunc (match, location) {
-    return match;
-  }
-
   render() {
     return (
       <nav className={styles.main}>
-        <a target="_blank" href="https://reactjs.org/" className={styles.link} rel='noopener noreferrer'>
+        <Link to="//www.google.com.co" className={styles.link} target="_blank" rel='noopener noreferrer'>
           <img src={logo} alt="wolox_logo" className={styles.logo} />
-        </a>
-        <NavLink exact to='/' className={`${styles.link}`} activeClassName={styles.active} isActive={this.isActiveFunc}>
-          Main
-        </NavLink>
-        <NavLink to='/about' className={styles.link} activeClassName={styles.active} isActive={this.isActiveFunc}>
-          About
-        </NavLink>
-        <NavLink to='/' className={`${styles.link} ${styles.signout}`} onClick={() => this.props.userLogout()}>
+        </Link>
+        <Link to='/' className={styles.link}>Main</Link>
+        <Link to='about' className={styles.link}>About</Link>
+        <Link to='/' className={`${styles.link} ${styles.signout}`} onClick={() => this.props.userLogout()}>
           <i className="fas fa-sign-out-alt"></i>
-        </NavLink>
+        </Link>
       </nav>
     );
   }
