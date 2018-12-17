18ad059203392d74a04809def0e2552ef01c36c1
Sebastian Zapata <sebastian.zapata@wolox.co>
2018-12-17 12:13:04 -0500

    previos NavLink

--- React/training/src/app/components/Topbar/index.js
+++ React/training/src/app/components/Topbar/index.js
@@ -3,10 +3,31 @@ import { connect } from 'react-redux';
 import styles from './styles.module.scss';
 import logo from './../../../assets/react_icon.svg';
 import authActions from './../../../redux/Auth/actions';
+import { NavLink } from "react-router-dom";
 
 class Topbar extends Component {
+
+  isActiveFunc (match, location) {
+    return match;
+  }
+
   render() {
     return (
+      <nav className={styles.main}>
+        <a target="_blank" href="https://reactjs.org/" className={styles.link} rel='noopener noreferrer'>
+          <img src={logo} alt="wolox_logo" className={styles.logo} />
+        </a>
+        <NavLink exact to='/' className={`${styles.link}`} activeClassName={styles.active} isActive={this.isActiveFunc}>
+          Main
+        </NavLink>
+        <NavLink to='/bla' className={styles.link} activeClassName={styles.active} isActive={this.isActiveFunc}>
+          Bla
+        </NavLink>
+        <NavLink to='/' className={`${styles.link} ${styles.signout}`} onClick={() => this.props.userLogout()}>
+          <i className="fas fa-sign-out-alt"></i>
+        </NavLink>
+      </nav>
+/*
       <nav className={styles.main}>
         <a href="https://reactjs.org/" target="_blank" className={styles.link}>
           <img src={logo} alt="wolox_logo" className={styles.logo} />
@@ -18,6 +39,7 @@ class Topbar extends Component {
           <i className="fas fa-sign-out-alt"></i>
         </a>
       </nav>
+*/
     );
   }
 }
--- React/training/src/app/index.js
+++ React/training/src/app/index.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Topbar from './components/Topbar';
 import { Provider } from 'react-redux';
 import store from './../redux/store.js';
-import Main from './screens/Main'
+import Main from './screens/Main';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+const Bla = () => (
+  <Fragment>
+    <Topbar />
+    <h1>Bla</h1>
+  </Fragment>
+);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Route exact path='/' component={Main}/>
+        <Route path='/bla' component={Bla}/>
+      </Router>
+    </Provider>
+  );
+}
+
+/*
 function App() {
   return (
     <Provider store={store}>
@@ -10,5 +31,6 @@ function App() {
     </Provider>
   );
 }
+*/
 
 export default App;
